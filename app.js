3c6dbfc3cbb0abf3148084905a7729db02a761f4
Jorge Martinez <jorgemartinez.ans@gmail.com>
2019-12-17 07:51:21 +1300

    fix

--- app.js
+++ app.js
@@ -2,23 +2,23 @@ import config from './config';
 import express from 'express';
 import morgan from 'morgan';
 import cors from 'cors';
+import path from 'path';
 
 const app = express();
-const path = require('path');
 
 // Middleware
 app.use(morgan('tiny'));
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+// app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/api', require('./routes/users'));
-
+  
 // Vue Middleware
 const history = require('connect-history-api-fallback');
 app.use(history());
 app.use(express.static(path.join(__dirname, 'public')));
-
 
 app.listen(config.port, (err, res) => {
     if (err) {
